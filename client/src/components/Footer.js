2b576d9b88f458437be658955b405d5a5f006d21
Adam Rahman <mrahmanadam@gmail.com>
2019-09-07 20:41:41 -0400

    Added example notes app + user authentication with JWT

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div>
+      <br />
+      <em>MERN boilerplate.</em>
+    </div>
+  )
+}
+
+export default Footer 
--- server/models/note.js
+++ server/models/note.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  date: Date,
-  important: Boolean,
-})
+    content: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    date: Date,
+    important: Boolean,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  })
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
